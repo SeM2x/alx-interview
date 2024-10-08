18f6e708fb79dce033ba928cc4629d6bfc919a7c
SeM2x <houssem135.302@gmail.com>
2024-10-08 09:28:57 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,15 +6,20 @@ def canUnlockAll(boxes):
     """Function to determine if all boxes can be opened"""
     if not boxes:
         return False
+
     unlocked = [0]
     keys = set(boxes[0])
 
     while keys:
         new_keys = set()
         for key in keys:
-            if key not in unlocked:
+            if key < len(boxes) and key not in unlocked:
                 unlocked.append(key)
                 new_keys.update(boxes[key])
+
+        if not new_keys:
+            break
+
         keys = new_keys
 
     return len(unlocked) == len(boxes)
