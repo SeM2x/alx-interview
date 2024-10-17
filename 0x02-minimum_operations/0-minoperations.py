1e1f98f059484f625dbc160ee518c5343ee6bd50
SeM2x <houssem135.302@gmail.com>
2024-10-17 16:27:52 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,36 +2,21 @@
 """module to define minOperations function"""
 
 
-def is_prime(n):
-    """function to check if a number is prime"""
-    if n < 2:
-        return False
-    for num in range(2, n):
-        if n % num == 0:
-            return False
-    return True
-
-
-def primes_list(n):
-    """function to generate a list of prime numbers"""
-    primes = []
-    for num in range(2, n + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
-
-
 def minOperations(n):
-    """function to calculate the minimum operations"""
+    """Function to calculate the minimum operations based on prime factors"""
     if n < 2:
         return 0
-    primes = primes_list(n)
-    nums = []
-    while n > 1:
-        for prime in primes:
-            if n % prime == 0:
-                nums.append(prime)
-                break
-        n = n // prime
 
-    return sum(nums)
+    num_operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            num_operations += factor
+            n = n // factor
+        factor += 1
+
+        if factor > 2 and factor % 2 == 0:
+            factor += 1
+
+    return num_operations
