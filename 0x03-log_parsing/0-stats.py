730f7878b448b0de894023dccfc841cebe21e4c8
SeM2x <houssem135.302@gmail.com>
2024-10-24 12:27:26 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,6 +3,25 @@
 """
 
 
+def get_stats(lines: list[str]):
+    """Print the total file size and status count"""
+    file_size = 0
+    status_count = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0,
+    }
+    for line in lines:
+        size = int(line.split(" ")[-1][:-1])
+        status = line.split(" ")[-2]
+        file_size += size
+        status_count[status] += 1
+
+    print(f"File size: {file_size}")
+    for key, val in status_count.items():
+        if val > 0:
+            print(f"{key}: ", val)
+
+
 if __name__ == '__main__':
     count = 0
     lines = []
