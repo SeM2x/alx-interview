730489f1faadd588ac5145723cba0fece4abab87
SeM2x <houssem135.302@gmail.com>
2024-10-24 12:14:10 +0100

    0-

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,35 +13,30 @@ def parse_line(line: str):
     """
     if not line or len(line) == 0:
         return False
-    ip = line.split('-')[0]
+    ip = line.split("-")[0]
     if len(ip) == 0:
         return False
-    date = line[line.find('[') + 1:line.find(']')]
+    date = line[line.find("[") + 1: line.find("]")]
     if len(date) == 0:
         return False
-    req = line[line.find('"') + 1:line.find('"', line.find('"') + 1)]
-    if req != 'GET /projects/260 HTTP/1.1':
+    req = line[line.find('"') + 1: line.find('"', line.find('"') + 1)]
+    if req != "GET /projects/260 HTTP/1.1":
         return False
-    status = line.split(' ')[-2]
+    status = line.split(" ")[-2]
     try:
         if int(status) not in [200, 301, 400, 401, 403, 404, 405, 500]:
             return False
     except Exception:
         return False
 
-    size = line.split(' ')[-1][:-1]
+    size = line.split(" ")[-1][:-1]
     try:
         int(size)
     except Exception:
         return False
 
-    return {
-        "ip": ip,
-        "date": date,
-        "request": req,
-        "status": status,
-        "size": int(size)
-    }
+    return {"ip": ip, "date": date, "request":
+            req, "status": status, "size": int(size)}
 
 
 def get_stats(lines: list[str]):
@@ -50,16 +45,24 @@ def get_stats(lines: list[str]):
     of each HTTP status code from a list of log lines.
     """
     file_size = 0
-    status_count = {"200": 0, "301": 0, "400": 0,
-                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    status_count = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
     for line in lines:
         file_size += parse_line(line)["size"]
         status_count[parse_line(line)["status"]] += 1
 
-    print(f'File size: {file_size}')
+    print(f"File size: {file_size}")
     for key, val in status_count.items():
         if val > 0:
-            print(f'{key}: ', val)
+            print(f"{key}: ", val)
 
 
 count = 0
