217161470bb738b9ea5a42744e197f8d43d36699
SeM2x <houssem135.302@gmail.com>
2024-10-24 12:26:15 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,3 +2,15 @@
 """A script for parsing HTTP request logs.
 """
 
+
+if __name__ == '__main__':
+    count = 0
+    lines = []
+    try:
+        while True:
+            lines.append(input())
+            count += 1
+            if count % 10 == 0:
+                get_stats(lines)
+    except (KeyboardInterrupt, EOFError):
+        get_stats(lines)
